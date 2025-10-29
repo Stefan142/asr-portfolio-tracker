d59336ffb5cef28584c1908ebb8a60a3d33f46bf
Stefan142 <stefan.marijnissen@student.uva.nl>
2025-10-29 11:54:37 +0100

    First version Asset python class.

---
+++ models/Asset.py
@@ -0,0 +1,44 @@
+import yfinance as yf
+
+
+class Asset:
+    def __init__(self, ticker, sector, asset_class, quantity, purchase_price):
+        self.ticker = ticker
+        self.name = yf.Ticker(self.ticker).info.get(
+            "longName",
+            f"No long name found for {self.ticker}",
+        )
+        self.sector = sector
+        self.asset_class = asset_class
+        self.quantity = [quantity]
+        self.purchase_price = [purchase_price]
+
+    def buy_more(self, quantity, price) -> None:
+        self.quantity.append(quantity)
+        self.purchase_price.append(price)
+
+    def last_price(self) -> float:
+        return yf.Ticker(self.ticker).fast_info.get("lastPrice", "No Last Price Found")
+
+    def transaction_values(self) -> list[float]:
+        """Total cost when bought"""
+        return [quantity*price for quantity, price in zip(self.quantity, self.purchase_price)]
+
+    def current_value(self) -> float:
+        """Most recent market value"""
+        return sum(self.quantity) * self.last_price()
+
+    def gain_loss(self) -> float:
+        last_price = self.last_price()
+        return sum(
+            [
+                last_price*quantity - transaction
+                for transaction, quantity in zip(self.transaction_values(), self.quantity)
+            ],
+        )
+
+    def retrieve_marketdata(self, date):
+        marketdata = yf.download(self.ticker, start=date)
+        marketdata = marketdata[["Close"]]
+        marketdata.columns = marketdata.columns.droplevel(['Ticker'])
+        return marketdata
