b38405f6e9534f6834c93913a33cfa18246c1f81
Stefan142 <stefanmarijnissen@yahoo.com>
2025-10-30 12:47:17 +0100

    Portfolio.py: Typehints + Docstrings

--- models/Portfolio.py
+++ models/Portfolio.py
@@ -1,12 +1,48 @@
 from models.Asset import Asset
+from typing import Optional, Tuple
 import pandas as pd
 import yfinance as yf
 
+
 class Portfolio:
+    """
+    A class for a portfolio, Stores assets and performs calculations.
+
+    Parameters
+    ----------
+    None
+
+    Attributes
+    ----------
+    assets: dict[str, Asset]
+        Storage for the assets.
+    """
     def __init__(self):
         self.assets = {}
     
-    def get_portfolio_weights(self, restrictions):
+    def get_portfolio_weights(
+            self,
+            restrictions: Optional[dict[str, str]],
+    ) -> Tuple[dict[str, float], float, float]:
+        """
+        Retrieves the relative weights of the assets under
+        "restrictions"
+
+        Parameters
+        ----------
+        restrictions: Optional[dict[str, str]]
+            Contains the Asset Class and/or the Sector.
+            Used to filter the Assets for the weight calculation.
+        
+        Returns
+        -------
+        Tuple[dict[str, float], float, float]
+            Relative weight of the asset w.r.t. asset class and/or
+            sector being the "total",
+            total value of the asset class and/or
+            sector w.r.t. the total portfolio, Value of the total
+            portfolio.
+        """
         total_value = sum([asset.current_value for ticker, asset in self.assets.items()])
         considered_assets = {}
         
@@ -33,13 +69,66 @@ class Portfolio:
         
         return relative_weights, total_relative_value, total_value
 
-    def add_new_asset(self, asset: Asset):
+    def add_new_asset(self, asset: Asset) -> None:
+        """
+        Adds a new asset to the portfolio.
+
+        Parameters
+        ----------
+        asset: models.Asset
+            The asset to add to the portfolio.
+
+        Returns
+        -------
+        None
+
+        Notes
+        -----
+        Adjusts self.assets
+        """
         self.assets[asset.ticker] = asset
     
-    def check_if_present(self, ticker):
+    def check_if_present(self, ticker: str) -> bool:
+        """
+        Checks if a ticker is present in self.assets
+
+        parameters
+        ----------
+        ticker: str
+            The ticker to check for.
+        
+        Returns
+        -------
+        bool
+            If it is present True, else False.
+        """
         return ticker in self.assets
-    
-    def get_portfolio_prices(self, restrictions, date1, date2=None):
+
+    def get_portfolio_prices(
+            self,
+            restrictions: Optional[dict[str, str]],
+            date1: str,
+            date2: Optional[str]=None,
+    ) -> pd.DataFrame:
+        """
+        Get prices of the portfolio (or a subset of it).
+        Portfolio is continuously rebalanced. (Weights stay the same)
+
+        Parameters
+        ----------
+        restrictions: Optional[dict[str, str]]
+            To filter the portfolio by asset class and/or sector.
+        date1: str
+            Starting date for the data.
+        date2: Optional[str]
+            Ending date for the data. ("None" retrieves most recent.)
+
+        Returns
+        -------
+        pd.DataFrame
+            A dataframe containing the NAV of
+            the rebalanced filtered portfolio.
+        """
         weights, _, _ = self.get_portfolio_weights(restrictions)
         tickers = list(weights.keys())
         marketData = yf.download(tickers, start=date1, end=date2, progress=False, auto_adjust=False)
@@ -47,25 +136,25 @@ class Portfolio:
         marketData.dropna(inplace=True)
         return marketData.mul(pd.Series(weights)).sum(axis=1).to_frame("Portfolio Price")
     
-    def delete_asset(self, ticker):
+    def delete_asset(self, ticker: str) -> None:
+        """
+        Deletes an asset from the portfolio.
+
+        Parameters
+        ----------
+        ticker: str
+            Ticker of the asset.
+
+        Returns
+        -------
+        None
+
+        Notes
+        -----
+        Prints to the terminal.
+        """
         if ticker in self.assets:
             del self.assets[ticker]
             print("Ticker deleted")
         else:
             print("Ticker not in portfolio, so no deletion.")
-
-
-
-
-# assets_dict = {
-#     "AAPL": Asset(ticker="AAPL", sector="Technology", asset_class="Equity", quantity=10, purchase_price=150.0),
-#     "MSFT": Asset(ticker="MSFT", sector="Technology", asset_class="Equity", quantity=8,  purchase_price=300.0),
-#     "JNJ":  Asset(ticker="JNJ",  sector="Healthcare", asset_class="Equity", quantity=15, purchase_price=160.0),
-#     "PG":   Asset(ticker="PG",   sector="Consumer", asset_class="Equity", quantity=20, purchase_price=140.0),
-#     "TLT":  Asset(ticker="TLT",  sector="Fixed Income", asset_class="Bond", quantity=5,  purchase_price=130.0),
-#     "GLD":  Asset(ticker="GLD",  sector="Commodities", asset_class="Commodity", quantity=12, purchase_price=170.0),
-# }
-
-# p = Portfolio(assets_dict)
-# restrictions = {"asset_class":"Equity", "sector":"Technology"}
-# print(p.get_portfolio_weights(restrictions))
